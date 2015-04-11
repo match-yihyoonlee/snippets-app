b2375ca0401d1c60fd429485de90cb15d020a1ef
Yih-Yoon Lee <yihyoon@gmail.com>
2015-04-11 08:32:56 -0500

    Create Put and Get functions

---
+++ snippets.py
@@ -0,0 +1,20 @@
+import logging
+
+# Set the log output file, and the log level
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name,snippet):
+	"""
+	Store a snippet with an associated name.
+	Return the name and the snippet
+	"""
+	logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+	return name, snippet
+
+def get(name):
+	""" Retrieve the snippet with a given name.
+	If there is no such snippet...
+	Return the snippet
+	"""
+	logging.error("FIXME: Unimplemented - get({!r})".format(name))
+	return name
